f7b67f0662a8ae003327871dc36eab8b18881826
pushkar <shajapurkarpushkar@gmail.com>
2022-12-28 19:27:40 +0530

    commit

--- gitdemo.py
+++ gitdemo.py
@@ -13,8 +13,5 @@ print("line 4 by push")
 print("line on local")
 print("line on local")
 print("line on local")
-<<<<<<< HEAD
 print("line on local")
-=======
 print("line on local")
->>>>>>> a3de81ad8363c1f27f50ed5ca4d4c4d70ddb1843
