0b39c39aebef1afc9851a008c5dee3592be90df3
pushkar <shajapurkarpushkar@gmail.com>
2022-12-29 18:09:45 +0530

    committed

---
+++ gitdemo.py
@@ -0,0 +1,17 @@
+print("Hello Everyone")
+print("ineuron")
+<<<<<<< HEAD
+print("line 3 by push")
+=======
+print ("line 3 ")
+>>>>>>> a46a7dab9b4c560055ea5eebccc4cf808c1d531f
+<<<<<<< HEAD
+print("line 4 ")
+=======
+print("line 4 by push")
+>>>>>>> 7b9a16d447402b4d9a43999f67132934eda6ec95
+print("line on local")
+print("line on local")
+print("line on local")
+print("line on local")
+print("line on local")
